f5139befe3955dde126c5905d2824c2d1f15ee74
Alex <alexbr784@gmail.com>
2024-08-10 12:55:31 +0200

    chore: Update Vite configuration to include base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/precio-luz-app/', // Asegúrate de que esta ruta sea correcta
   plugins: [react()],
-})
+});
