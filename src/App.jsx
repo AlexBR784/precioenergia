e30b1b2d7d6254fef68359a3f04356d2248a72ab
Alex <alexbr784@gmail.com>
2024-08-10 12:33:08 +0200

    chore: Add Vite configuration and setup React plugin

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useEnergyCost } from "./hooks/useEnergyCost";
+import {
+  CircularProgress,
+  Card,
+  Chip,
+  CardContent,
+  Alert,
+  Typography,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { BarChart } from "@mui/x-charts";
+
+function App() {
+  const { energyCost, loading, cheapPrice } = useEnergyCost();
+
+  const chartSetting = {
+    xAxis: [
+      {
+        label: "Coste €/MWh",
+      },
+    ],
+    width: 500,
+    height: 400,
+  };
+
+  const getColor = (price) => {
+    // "#9ADE7B" : "#FF8F8F"
+    if (price == cheapPrice) {
+      return "#98D8AA";
+    } else if (price > cheapPrice && price <= cheapPrice * 1.5) {
+      return "#F3E99F";
+    } else if (price > cheapPrice * 1.5 && price < cheapPrice * 2) {
+      return "#F7D060";
+    } else {
+      return "#FF6D60";
+    }
+  };
+
+  let xAxisData = [];
+  let yAxisData = [];
+  let colors = [];
+
+  energyCost?.map((item) => {
+    xAxisData.push(item.price);
+    yAxisData.push(item.hour);
+    colors.push(getColor(item.price));
+  });
+
+  // Get the minimum price and assoiated hour
+  const minPrice = Math.min(...xAxisData);
+  const minPriceIndex = xAxisData.indexOf(minPrice);
+
+  return (
+    <section>
+      <Card variant="outlined">
+        <CardContent>
+          <Typography variant="h6">Coste por hora</Typography>
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <BarChart
+              yAxis={[
+                {
+                  scaleType: "band",
+                  data: yAxisData,
+                  colorMap: {
+                    type: "ordinal",
+                    colors: colors,
+                  },
+                },
+              ]}
+              series={[
+                {
+                  data: xAxisData,
+                  type: "bar",
+                },
+              ]}
+              layout="horizontal"
+              grid={{ vertical: true }}
+              {...chartSetting}
+            />
+          )}
+
+          <Alert severity="info">{`El precio más bajo es de ${cheapPrice}€/MWh de las ${yAxisData[minPriceIndex]}h.`}</Alert>
+        </CardContent>
+      </Card>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/hooks/useEnergyCost.js
@@ -0,0 +1,49 @@
+// Custom hook that uses the energy cost API to get the energy cost for today
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useEnergyCost = () => {
+    const [energyCost, setEnergyCost] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [cheapPrice, setCheapPrice] = useState(true);
+
+    useEffect(() => {
+        async function fetchEnergyCost() {
+
+            const targetUrl = 'https://corsproxy.io/?https://api.preciodelaluz.org/v1/prices/all?zone=PCB';
+
+            const resp = await axios.get(targetUrl);
+
+            const parsedData = [];
+            const isCheap = [];
+
+            // Loop through each item in the response and return the price
+            Object.keys(resp.data).forEach((key) => {
+                const item = resp.data[key];
+                if (item['is-cheap']) {
+                    isCheap.push(item)
+                }
+                // From the cheapest items, get the cheaper
+                parsedData.push(item)
+
+            });
+
+            setEnergyCost(parsedData);
+            setLoading(false);
+
+            if (isCheap.length > 0) {
+                const cheaper = isCheap.reduce((prev, current) => {
+                    return (prev.price < current.price) ? prev : current
+                });
+                setCheapPrice(cheaper.price);
+            }
+
+        }
+        setLoading(true);
+        fetchEnergyCost();
+
+    }, []);
+
+    return { energyCost, loading, cheapPrice };
+}
