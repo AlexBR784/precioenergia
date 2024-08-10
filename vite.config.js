1a4f2f24b6ece59408de4b4ab147259a4a82c3fe
Alex <alexbr784@gmail.com>
2024-08-10 12:57:25 +0200

    feat: Update base path and asset URLs for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/precio-luz-app/', // Asegúrate de que esta ruta sea correcta
+  base: '/precioenergia/', // Asegúrate de que esta ruta sea correcta
   plugins: [react()],
 });
